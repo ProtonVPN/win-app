41ecd660099f695550aebf27af928f5941e7bd76
Mindaugas Veblauskas <mindaugas.veblauskas@protonmail.com>
2020-05-04 07:42:03 +0300

    Remove tmp path from gosrp script

--- src/ProtonVPN.App/gosrp.bat
+++ src/ProtonVPN.App/gosrp.bat
@@ -2,7 +2,6 @@ pushd ..\srp\windows\cshared
 set fn=GoSrp
 set gn=main.go
 set CGO_ENABLED=1
-set tmp=%path%
 
 set GOARCH=386
 go build -buildmode=c-shared -v -ldflags="-s -w" -o ..\..\..\bin\x86\%fn%.dll %gn%
