a23d3b08ddc26dc8d863a34aabedbe7b04ffe271
Mindaugas Veblauskas <mindaugas.veblauskas@protonmail.com>
2020-07-17 09:09:59 +0300

    Fix installer launcher on CI runner

--- ci/launch-installer.ps1
+++ ci/launch-installer.ps1
@@ -1,7 +1,6 @@
 $version = (get-item .\src\bin\ProtonVPN.exe).VersionInfo | % {("{0}.{1}.{2}" -f $_.FileMajorPart,$_.FileMinorPart,$_.FileBuildPart)}
-$hash = $env:CI_COMMIT_SHORT_SHA
 $projectDir = $env:CI_PROJECT_DIR
-$installerPath = $projectDir + "\Setup\ProtonVPN-SetupFiles\ProtonVPN_win_v" + $version + "-" + $hash + ".exe"
+$installerPath = $projectDir + "\Setup\ProtonVPN-SetupFiles\ProtonVPN_win_v" + $version + ".exe"
 
 Write-Output ("Write event with installer path: " + $installerPath)
 
