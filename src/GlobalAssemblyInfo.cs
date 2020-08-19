937035fb2cccf651b01d1a5382f2a2b8bd1f6ca3
Mindaugas Veblauskas <mindaugas.veblauskas@protonmail.com>
2020-08-19 12:15:06 +0300

    Increase app version

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.16.3.0")]
-[assembly: AssemblyFileVersion("1.16.3.0")]
+[assembly: AssemblyVersion("1.17.0.0")]
+[assembly: AssemblyFileVersion("1.17.0.0")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyInformationalVersion("$AssemblyVersion")]
