47c970ab7f200dbcaca42719b9e8da495847f93e
Mindaugas Veblauskas <mindaugas.veblauskas@protonmail.com>
2020-09-17 12:23:28 +0300

    Add reboot warning modal to the installer

--- src/ProtonVPN.InstallActions/Utils.h
+++ src/ProtonVPN.InstallActions/Utils.h
@@ -7,3 +7,4 @@ void SetMsiHandle(MSIHANDLE msiHandle);
 void LogMessage(std::wstring message, int result);
 void LogMessage(std::wstring message);
 std::wstring GetProperty(std::wstring name);
+void SetProperty(const std::wstring name, const std::wstring value);
