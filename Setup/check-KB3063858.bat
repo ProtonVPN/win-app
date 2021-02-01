915b729ca9cbedf9686ee9ffcbf57a7bbb7150c1
Mindaugas Veblauskas <mindaugas.veblauskas@protonmail.com>
2021-02-01 16:31:33 +0200

    Skip KB3063858 if later updates installed

--- Setup/check-KB3063858.bat
+++ Setup/check-KB3063858.bat
@@ -1,6 +1,6 @@
 @echo off
 
-wmic qfe get hotfixid | FindStr "KB3063858" > NUL
+wmic qfe get hotfixid | FindStr "KB3063858 KB2533623 KB4457144 KB3126587 KB3126593 KB3146706 KB4014793" > NUL
 if errorlevel 1 (
 	echo Update is not installed
 	exit /b %errorlevel%
