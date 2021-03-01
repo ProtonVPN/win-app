8ddff09c11e873f6354006687d5d6ee07c522016
Mindaugas Veblauskas <mindaugas.veblauskas@protonmail.com>
2021-03-01 11:02:48 +0200

    Add binary for removing wfp filters

---
+++ ProtonVPN.RestoreInternet/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ProtonVPN.RestoreInternet
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Clearing wfp filters...");
+            uint result = RemoveWfpObjects(0);
+            if (result == 0)
+            {
+                Console.WriteLine("OK");
+            }
+            else
+            {
+                Console.WriteLine("Error: " + result);
+            }
+
+            Console.WriteLine("Hit enter to close this window");
+            Console.ReadLine();
+        }
+
+        [DllImport(
+            "ProtonVPN.InstallActions.dll",
+            EntryPoint = "RemoveWfpObjects",
+            CallingConvention = CallingConvention.Cdecl)]
+        public static extern uint RemoveWfpObjects(long handle);
+    }
+}
