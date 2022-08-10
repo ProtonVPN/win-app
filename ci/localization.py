0fe55090620f4ec184bd48f0ec9fb40633916e1b
Aurelien <agarroux+gitlab@proton.ch>
2022-08-10 12:25:19 +0000

    Add flow MR translations

--- ci/localization.py
+++ ci/localization.py
@@ -1,39 +1,16 @@
-import os
-import re
 import subprocess
 from pathlib import Path
+from typing import Tuple, List
 
-class Localization:
-    src = ''
-    dst = ''
-    validator = ''
 
-    def __init__(self, src, dst, validator):
-        self.src = src
-        self.dst = dst
-        self.validator = validator
+def lint(linter: Path, sources: Path) -> Tuple[int, List[Path]]:
+    """
+    Lint translations files to ensure their content is valid resx
+    """
+    errors = []
+    for file in sources.glob("*.resx"):
+        code = subprocess.call(f'{linter} "{file}"', shell=True)
+        if code != 0:
+            errors.append(file)
 
-    def AddLanguages(self):
-        returnCode = 0
-        for f in list(Path().glob(self.src)):
-            lang = self.getLanguageCode(f)
-            if self.validate(f.absolute()):
-                self.AddLanguage(f)
-            else:
-                returnCode = 1
-                break
-        return returnCode
-
-    def validate(self, file):
-        print(file)
-        code = subprocess.call("{validator} \"{file}\"".format(validator=self.validator, file=file, shell=True))
-        return code == 0
-
-    def AddLanguage(self, src):
-        lang = self.getLanguageCode(src)
-        dest = self.dst.format(lang=lang)
-        os.replace(str(src), dest)
-
-    def getLanguageCode(self, path):
-        match = re.match('.+ProtonVPN Windows app\.(.+)\.resx', str(path))
-        return match.group(1)
+    return (int(bool(len(errors))), errors)
