fb37d033083638ce574eaf483ac88acf8ccc78f6
Mindaugas Veblauskas <mindaugas.veblauskas@protonmail.com>
2022-08-31 13:05:21 +0300

    Build LocalAgent.dll on BuildDependencies.bat

--- BuildDependencies.bat
+++ BuildDependencies.bat
@@ -20,7 +20,7 @@ msbuild src\ProtonVPN.NetworkUtil\ProtonVPN.NetworkUtil.vcxproj /p:PlatformTools
 msbuild src\ProtonVPN.InstallActions\ProtonVPN.InstallActions.vcxproj /p:PlatformToolset=%platformToolset% /p:Platform=Win32 /p:Configuration=Release
 
 ::GoSrp.dll
-pushd src\srp\windows\cshared
+pushd %~dp0\src\srp\windows\cshared
 set fn=GoSrp
 set gn=main.go
 set CGO_ENABLED=1
@@ -30,3 +30,16 @@ go build -buildmode=c-shared -v -ldflags="-s -w" -o ..\..\..\bin\x86\%fn%.dll %g
 
 set GOARCH=amd64
 go build -buildmode=c-shared -v -ldflags="-s -w" -o ..\..\..\bin\x64\%fn%.dll %gn%
+
+::LocalAgent.dll
+pushd %~dp0\src\ProtonVPN.LocalAgent\localAgentWin
+set GOOS=windows
+set GO111MODULE=off
+set CGO_CFLAGS=-O3 -Wall -Wno-unused-function -Wno-switch -std=gnu11 -DWINVER=0x0601
+set CC=x86_64-w64-mingw32-gcc
+
+set GOARCH=386
+go build -buildmode c-shared -ldflags="-w -s" -trimpath -v -o "..\..\bin\Resources\32-bit\LocalAgent.dll" || exit /b 1
+
+set GOARCH=amd64
+go build -buildmode c-shared -ldflags="-w -s" -trimpath -v -o "..\..\bin\Resources\64-bit\LocalAgent.dll" || exit /b 1
