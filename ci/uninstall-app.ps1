94458c7c048dcebb865edac147ca65e83d1fed33
Lukas Makaravičius <lukas.makaravicius@protonmail.com>
2022-09-22 08:47:41 +0300

    Delete app folder after app uninstall in UI tests

--- ci/uninstall-app.ps1
+++ ci/uninstall-app.ps1
@@ -1,7 +1,16 @@
+$protonVpnString = "ProtonVPN"
 $app = Get-WmiObject -Class Win32_Product | Where-Object {
-    $_.Name -match "ProtonVPN"   
+    $_.Name -match $protonVpnString
 }
 
-if($app){
+$appPath = ($app | Where-Object {
+    $_.Name -eq $protonVpnString
+}).InstallLocation
+
+if ($app) {
     $output = $app.Uninstall();
+}
+
+if (Test-Path $appPath) {
+    Remove-Item $appPath -Recurse
 }
