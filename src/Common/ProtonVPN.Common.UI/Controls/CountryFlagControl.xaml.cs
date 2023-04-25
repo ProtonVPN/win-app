5baae1e4201b262c7f64c9aa9c53d668ad7dbd10
Thomas Felices <thomas.felices@proton.ch>
2023-04-25 15:26:04 +0000

    Add support for Flags (svg resources) [VPNWIN-1454]

--- src/Common/ProtonVPN.Common.UI/Controls/CountryFlagControl.xaml.cs
+++ src/Common/ProtonVPN.Common.UI/Controls/CountryFlagControl.xaml.cs
@@ -17,14 +17,25 @@
  * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+using System;
+using System.IO;
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Media.Imaging;
 
 namespace ProtonVPN.Common.UI.Controls;
 
+[TemplatePart(Name = "PART_ExitCountryFlag", Type = typeof(Image))]
+[TemplatePart(Name = "PART_MiddleCountryFlag", Type = typeof(Image))]
 public sealed partial class CountryFlagControl
 {
+    private const string FLAG_ASSETS_FOLDER_PATH = "ms-appx:///ProtonVPN.Common.UI/Assets/Flags/";
+    private const string FLAG_ASSETS_FILE_EXTENSION = ".svg";
+    private const string FLAG_ASSETS_FASTEST = "Fastest";
+    private const string FLAG_ASSETS_PLACEHOLDER = "Placeholder";
+
     public static readonly DependencyProperty CountryCodeProperty =
-        DependencyProperty.Register(nameof(CountryCode), typeof(string), typeof(CountryFlagControl), new PropertyMetadata(default));
+        DependencyProperty.Register(nameof(CountryCode), typeof(string), typeof(CountryFlagControl), new PropertyMetadata(default, OnCountryCodePropertyChanged));
 
     public static readonly DependencyProperty MiddleCountryCodeProperty =
         DependencyProperty.Register(nameof(MiddleCountryCode), typeof(string), typeof(CountryFlagControl), new PropertyMetadata(default, OnMiddleCountryCodePropertyChanged));
@@ -64,10 +75,19 @@ public sealed partial class CountryFlagControl
         set => SetValue(IsCompactProperty, value);
     }
 
+    private static void OnCountryCodePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is CountryFlagControl control)
+        {
+            control.UpdateCountryFlag();
+        }
+    }
+
     private static void OnMiddleCountryCodePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
         if (d is CountryFlagControl control)
         {
+            control.UpdateMiddleCountryFlag();
             control.InvalidateFlagsLayout();
         }
     }
@@ -106,5 +126,37 @@ public sealed partial class CountryFlagControl
 
         VisualStateManager.GoToState(this, secureCoreVisualState, false);
         VisualStateManager.GoToState(this, flagLayoutVisualState, false);
+    }
+
+    private void UpdateCountryFlag()
+    {
+        PART_ExitCountryFlag.Source = GetImageSource(CountryCode);
+    }
+
+    private void UpdateMiddleCountryFlag()
+    {
+        PART_MiddleCountryFlag.Source = GetImageSource(MiddleCountryCode);
+    }
+
+    private SvgImageSource GetImageSource(string countryCode)
+    {
+        if (string.IsNullOrEmpty(countryCode))
+        {
+            countryCode = FLAG_ASSETS_FASTEST;
+        }
+
+        Uri uri = BuildUri(countryCode);
+
+        if (!File.Exists(AppDomain.CurrentDomain.BaseDirectory + uri.LocalPath))
+        {
+            uri = BuildUri(FLAG_ASSETS_PLACEHOLDER);
+        }
+
+        return new SvgImageSource(uri);
+    }
+
+    private Uri BuildUri(string resourceName)
+    {
+        return new(Path.Combine(FLAG_ASSETS_FOLDER_PATH, resourceName + FLAG_ASSETS_FILE_EXTENSION));
     }
 }
