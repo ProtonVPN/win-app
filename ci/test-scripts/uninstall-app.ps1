89cebb7260047189647cb3f74dc512ef1cd24913
Lukas <lukas.makaravicius@proton.ch>
2023-08-14 11:31:26 +0300

    Check also if uninstaller path exists

--- ci/test-scripts/uninstall-app.ps1
+++ ci/test-scripts/uninstall-app.ps1
@@ -1,6 +1,8 @@
 $protonFolder = "C:\Program Files\Proton\VPN"
 $protonUninstallExe = $protonFolder + "\unins000.exe"
-if (Test-Path -Path $protonFolder) {
+if(Test-Path -Path $protonUninstallExe) {
     Start-Process -FilePath $protonUninstallExe -ArgumentList "/verysilent" -Wait -ErrorAction Ignore
+}
+if (Test-Path -Path $protonFolder) {
     Remove-Item $protonFolder -Recurse -ErrorAction Ignore
 }
