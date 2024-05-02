6176e65c9ec818850ff75188f883e55400bd1e86
Mindaugas Veblauskas <mindaugas.veblauskas@proton.ch>
2024-05-02 08:17:30 +0300

    Update OpenVPN to 2.6.10 [VPNWIN-2191]

---
+++ src/ProtonVPN.Vpn/OpenVpn/Arguments/WindowsDriverArgument.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Copyright (c) 2024 Proton AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ProtonVPN.Vpn.OpenVpn.Arguments;
+
+public class WindowsDriverArgument : IEnumerable<string>
+{
+    private readonly string _driver;
+
+    public WindowsDriverArgument(string driver)
+    {
+        _driver = driver;
+    }
+
+    public IEnumerator<string> GetEnumerator()
+    {
+        yield return $"--windows-driver {_driver}";
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
