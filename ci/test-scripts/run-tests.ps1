242e5597e363461d9f3a48d22e4fb85f360f57ad
Lukas <lukas.makaravicius@proton.ch>
2024-08-12 15:18:58 +0300

    Add 30 minutes timeout for UI test run

--- ci/test-scripts/run-tests.ps1
+++ ci/test-scripts/run-tests.ps1
@@ -2,11 +2,12 @@ param (
     [string]$Category
 )
 
-$output = & VSTest.Console.exe src\bin\e2e\ProtonVPN.UI.Tests.dll /TestCaseFilter:"Category=$Category"
+$output = & VSTest.Console.exe src\bin\e2e\ProtonVPN.UI.Tests.dll /Settings:.testsettings.xml /TestCaseFilter:"Category=$Category"
 $exitCode = $LASTEXITCODE
 
 $keywords = @("BVI-", "BackdropLocal", "worldTransform", "0.00, 0.00")
 
+#Filter noise from some of the runners
 $filteredOutput = $output | Where-Object {
     $line = $_
     $shouldDrop = $false
