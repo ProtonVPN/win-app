33bca6216ba7ebd3ef834af2c08121a7cebb80bf
Thomas Felices <thomas.felices@proton.ch>
2024-10-17 05:36:38 +0000

    Update and add new icons [VPNWIN-2395]

---
+++ src/Client/Common/ProtonVPN.Client.Common.UI/Assets/Icons/PathIcons/Sparks.cs
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 Proton AG
+ *
+ * This file is part of ProtonVPN.
+ *
+ * ProtonVPN is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ProtonVPN is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using ProtonVPN.Client.Common.UI.Assets.Icons.Base;
+
+namespace ProtonVPN.Client.Common.UI.Assets.Icons.PathIcons;
+
+public class Sparks : CustomPathIcon
+{
+    protected override string IconGeometry16 { get; }
+        = "M11.8159 2.05127L12.2625 0.71147C12.3385 0.483546 12.6609 0.483546 12.7369 0.71147L13.1835 2.05127C13.3825 2.64848 13.8512 3.11712 14.4484 3.31619L15.7882 3.76279C16.0161 3.83876 16.0161 4.16115 15.7882 4.23713L14.4484 4.68373C13.8512 4.8828 13.3825 5.35143 13.1835 5.94864L12.7369 7.28845C12.6609 7.51637 12.3385 7.51637 12.2625 7.28845L11.8159 5.94864C11.6169 5.35143 11.1482 4.8828 10.551 4.68373L9.21122 4.23713C8.98329 4.16115 8.98329 3.83876 9.21122 3.76279L10.551 3.31619C11.1482 3.11712 11.6169 2.64848 11.8159 2.05127Z M4.49609 7.19712L5.35038 4.95451C5.43402 4.73495 5.74688 4.74156 5.82118 4.96445L6.52535 7.07707C6.82396 7.97295 7.52697 8.67593 8.42285 8.97452L10.5352 9.67855C10.7581 9.75284 10.7648 10.0657 10.5452 10.1493L8.30268 11.0036C7.47647 11.3184 6.83116 11.9816 6.53909 12.816L5.82006 14.8704C5.74373 15.0885 5.43778 15.0952 5.35197 14.8807L4.47721 12.6938C4.17228 11.9315 3.56826 11.3274 2.80595 11.0225L0.619069 10.1478C0.404544 10.0619 0.411257 9.75598 0.629339 9.67966L2.68354 8.96079C3.51811 8.66874 4.18134 8.02339 4.49609 7.19712Z";
+
+    protected override string IconGeometry20 { get; }
+        = ""; 
+
+    protected override string IconGeometry24 { get; }
+        = ""; 
+
+    protected override string IconGeometry32 { get; }
+        = "";
+}
