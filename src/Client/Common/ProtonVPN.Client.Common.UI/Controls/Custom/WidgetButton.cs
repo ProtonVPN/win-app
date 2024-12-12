752a60c36b51af020170ccf5a201e39d1ad5fec8
Thomas Felices <thomas.felices@proton.ch>
2024-12-12 12:00:28 +0100

    Add delay to feature flyouts

--- src/Client/Common/ProtonVPN.Client.Common.UI/Controls/Custom/WidgetButton.cs
+++ src/Client/Common/ProtonVPN.Client.Common.UI/Controls/Custom/WidgetButton.cs
@@ -17,13 +17,19 @@
  * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+using System;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Input;
+using Windows.Foundation;
 
 namespace ProtonVPN.Client.Common.UI.Controls.Custom;
 
 public class WidgetButton : Button
 {
+    private const int OPEN_FLYOUT_DELAY_IN_MS = 400;
+
     public static readonly DependencyProperty IsActiveProperty =
         DependencyProperty.Register(nameof(IsActive), typeof(bool), typeof(WidgetButton), new PropertyMetadata(default));
 
@@ -40,5 +46,84 @@ public class WidgetButton : Button
     {
         get => (string)GetValue(TextProperty);
         set => SetValue(TextProperty, value);
+    }
+
+    private DispatcherTimer _timer ;
+
+    public WidgetButton()
+    {
+        _timer = new DispatcherTimer() 
+        { 
+            Interval = TimeSpan.FromMilliseconds(OPEN_FLYOUT_DELAY_IN_MS) 
+        };
+        _timer.Tick += OnOpenFlyoutTimerTick;
+    }
+
+    protected override void OnPointerEntered(PointerRoutedEventArgs e)
+    {
+        base.OnPointerEntered(e);
+
+        if (Flyout == null)
+        {
+            return;
+        }
+
+        if (!Flyout.IsOpen)
+        {
+            StartTimer();
+        }
+        else
+        {
+            // When pointer is over the button, switch to transient mode so the flyout cannot be dismissed
+            Flyout.ShowMode = FlyoutShowMode.Transient;
+        }
+    }
+
+    protected override void OnPointerExited(PointerRoutedEventArgs e)
+    {
+        base.OnPointerExited(e);
+
+        if (Flyout == null)
+        {
+            return;
+        }
+
+        StopTimer();
+
+        if (Flyout.IsOpen)
+        {
+            // When pointer is not over the button, flyout can be dismissed when the pointer moves away
+            Flyout.ShowMode = FlyoutShowMode.TransientWithDismissOnPointerMoveAway;
+        }
+    }
+
+    private void OnOpenFlyoutTimerTick(object? sender, object e)
+    {
+
+        StopTimer();
+
+        FlyoutShowOptions options = new()
+        {
+            ShowMode = FlyoutShowMode.Transient,
+            Placement = FlyoutPlacementMode.LeftEdgeAlignedTop,
+            Position = new Point(-16, -1)
+        };
+        Flyout.ShowAt(this, options);
+    }
+
+    private void StartTimer()
+    {
+        if (!_timer.IsEnabled)
+        {
+            _timer.Start();
+        }
+    }
+
+    private void StopTimer()
+    {
+        if (_timer.IsEnabled)
+        {
+            _timer.Stop();
+        }
     }
 }
--- src/Client/ProtonVPN.Client/UI/Main/Features/NetShield/NetShieldWidgetView.xaml.cs
+++ src/Client/ProtonVPN.Client/UI/Main/Features/NetShield/NetShieldWidgetView.xaml.cs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Proton AG
+ * Copyright (c) 2024 Proton AG
  *
  * This file is part of ProtonVPN.
  *
@@ -18,9 +18,6 @@
  */
 
 using ProtonVPN.Client.Core.Bases;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Input;
-using Windows.Foundation;
 
 namespace ProtonVPN.Client.UI.Main.Features.NetShield;
 
@@ -38,34 +35,6 @@ public sealed partial class NetShieldWidgetView : IContextAware
     public object GetContext()
     {
         return ViewModel;
-    }
-
-    private void OnWidgetButtonPointerEntered(object sender, PointerRoutedEventArgs e)
-    {
-        if (!ViewModel.IsFeatureFlyoutOpened)
-        {
-            FlyoutShowOptions options = new()
-            {
-                ShowMode = FlyoutShowMode.Transient,
-                Placement = FlyoutPlacementMode.LeftEdgeAlignedTop,
-                Position = new Point(-16, -1)
-            };
-            WidgetFlyout.ShowAt(WidgetButton, options);
-        }
-        else
-        {
-            // When pointer is over the button, switch to transient mode so the flyout cannot be dismissed
-            WidgetFlyout.ShowMode = FlyoutShowMode.Transient;
-        }
-    }
-
-    private void OnWidgetButtonPointerExited(object sender, PointerRoutedEventArgs e)
-    {
-        if (ViewModel.IsFeatureFlyoutOpened)
-        {
-            // When pointer is not over the button, flyout can be dismissed when the pointer moves away
-            WidgetFlyout.ShowMode = FlyoutShowMode.TransientWithDismissOnPointerMoveAway;
-        }
     }
 
     private void OnWidgetFlyoutOpened(object sender, object e)
