53e7481385ab1b6e4b0f3a46cadf8b89f130d870
Eduardo Abreu <eduardo.abreu@proton.ch>
2025-04-15 07:39:59 +0000

    Fix Guest Holes CI job

--- ci/build-scripts/guest_hole_server_loader.py
+++ ci/build-scripts/guest_hole_server_loader.py
@@ -1,10 +1,10 @@
+import os
 import random
 import requests
 
 def load():
     # Constants
 
-    server_list_api_url: str = 'https://vpn-api.proton.me/vpn/logicals?SignServer=Server.EntryIP,Server.Label'
     num_guest_hole_servers: int = 10
     secure_core_flag: int = 1
 
@@ -12,46 +12,11 @@ def load():
 
     print('Started updating guest hole servers json')
 
-    proxies = {
-        'http': 'http://proxy.plabs.ch:3128',
-        'https': 'http://proxy.plabs.ch:3128',
-    }
-
-    response = requests.get(server_list_api_url, proxies=proxies)
-    original_json = response.json()
-
-    logical_servers = original_json["LogicalServers"]
-    print(''.join(['Number of logical servers: ', str(len(logical_servers))]))
-
-    non_secure_core_logical_servers = [logical_server for logical_server in logical_servers
-                                       if (logical_server['Features'] & secure_core_flag) == 0]
-    print(''.join(['Number of non Secure Core logical servers: ', str(len(non_secure_core_logical_servers))]))
-
-    random.shuffle(non_secure_core_logical_servers)
-
-    servers_str: str = ""
-    unique_entries: list = []
-
-    for logical_server in non_secure_core_logical_servers:
-        server = logical_server['Servers'][0]
-        if server['EntryIP'] in unique_entries:
-            continue
-
-        server_json_object = "".join([
-            '{"host":"', server['Domain'],
-            '","ip":"', server['EntryIP'],
-            '","signature":"', server['Signature'],
-            '","label":"', server['Label'],
-            '","publicKey":"', server['X25519PublicKey'], '"}'])
-        servers_str = ",".join([servers_str, server_json_object])
-        unique_entries.append(server['EntryIP'])
-
-        if len(unique_entries) >= num_guest_hole_servers:
-            break
-
-    servers_str = servers_str.lstrip(',')
+    servers_str: str = os.getenv('GUEST_HOLE_LOGICALS')
+    servers_str = servers_str.strip()
+    servers_str = servers_str.strip(',')
 
     with open(r'.\Setup\GuestHoleServers.json', 'w') as file:
-        file.write("".join(['[', servers_str, ']']))
+        file.write(servers_str)
 
     print('Finished updating guest hole servers json')
