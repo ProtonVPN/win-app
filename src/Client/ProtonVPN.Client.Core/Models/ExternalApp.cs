27bdfffb8f94fd8eca981a3cbb8a2004fb82f63e
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-02 03:54:21 -0400

    Allow tunneling app models to customize validity

--- src/Client/ProtonVPN.Client.Core/Models/ExternalApp.cs
+++ src/Client/ProtonVPN.Client.Core/Models/ExternalApp.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * Copyright (c) 2025 Proton AG
  *
  * This file is part of ProtonVPN.
@@ -36,7 +36,7 @@ public class ExternalApp : ObservableObject
     public string AppName { get; }
     public ImageSource? AppIcon { get; private set; }
 
-    public bool IsValid => IsAppPathValid(AppPath);
+    public virtual bool IsValid => IsAppPathValid(AppPath);
 
     protected ExternalApp(string appPath, string appName, ImageSource? appIcon)
         : this(appPath, appName)
