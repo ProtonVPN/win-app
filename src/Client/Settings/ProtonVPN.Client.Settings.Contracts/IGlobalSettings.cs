1d0e6d77f27013b2dd6871ed20b7e699e9689c2b
marcmy <21000174+marcmy@users.noreply.github.com>
2026-06-04 01:34:49 -0400

    Add skipped update version setting

--- src/Client/Settings/ProtonVPN.Client.Settings.Contracts/IGlobalSettings.cs
+++ src/Client/Settings/ProtonVPN.Client.Settings.Contracts/IGlobalSettings.cs
@@ -1,22 +1,3 @@
-﻿/*
- * Copyright (c) 2023 Proton AG
- *
- * This file is part of ProtonVPN.
- *
- * ProtonVPN is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * ProtonVPN is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
- */
-
 using System.Collections.Concurrent;
 using ProtonVPN.Client.Settings.Contracts.Enums;
 using ProtonVPN.Client.Settings.Contracts.Models;
@@ -49,6 +30,7 @@ public interface IGlobalSettings
     bool IsKillSwitchEnabled { get; set; }
     bool IsBetaAccessEnabled { get; set; }
     bool AreAutomaticUpdatesEnabled { get; set; }
+    string? SkippedUpdateVersion { get; set; }
     bool IsGlobalSettingsMigrationDone { get; set; }
     KillSwitchMode KillSwitchMode { get; set; }
     List<FeatureFlag> FeatureFlags { get; set; }
