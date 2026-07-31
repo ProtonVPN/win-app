eb6287db96e4fa4723679cb5c76e53f0eb0ec256
Mindaugas Veblauskas <mindaugas.veblauskas@proton.ch>
2026-07-31 12:05:18 +0300

    Fix reconnect after sleep [VPNWIN-3348]

--- src/Client/ProtonVPN.Client/Handlers/PowerModeAndWindowActivationHandler.cs
+++ src/Client/ProtonVPN.Client/Handlers/PowerModeAndWindowActivationHandler.cs
@@ -82,7 +82,12 @@ public class PowerModeAndWindowActivationHandler : IHandler,
         if (_userAuthenticator.IsLoggedIn)
         {
             _logger.Info<AppLog>("Resuming from sleep while logged in");
-            _connectionManager.ReconnectAsync(VpnTriggerDimension.Auto);
+
+            if (_connectionManager.IsConnected)
+            {
+                _connectionManager.ReconnectAsync(VpnTriggerDimension.Auto);
+            }
+
             OnResumeOrWindowActivation();
         }
         else
